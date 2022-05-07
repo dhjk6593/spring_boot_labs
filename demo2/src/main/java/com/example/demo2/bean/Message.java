5b5107fe61ae4b76e40eee4bd8af5e329aeab441
Ella Chang <dhjk65493@gmail.com>
2022-05-07 15:54:58 +0800

    add 2 rest controllers

---
+++ demo2/src/main/java/com/example/demo2/bean/Message.java
@@ -0,0 +1,29 @@
+package com.example.demo2.bean;
+
+public class Message {
+
+    private int id;
+
+    private String name;
+
+    public Message(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
