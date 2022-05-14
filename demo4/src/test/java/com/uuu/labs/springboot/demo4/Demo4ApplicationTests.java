734c13dc39df212c23f2a9291de570487c5b8055
Ella Chang <dhjk65493@gmail.com>
2022-05-14 10:18:45 +0800

    jdbc example from project template

---
+++ demo4/src/test/java/com/uuu/labs/springboot/demo4/Demo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uuu.labs.springboot.demo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
